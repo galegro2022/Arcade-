d767e1b76b780ab21dc67d28afd9ae627da02f3e
Alegro Adimora Godley Godley <alegroadimoragodleygodley@Alegros-MacBook-Pro.local>
2023-01-06 15:34:24 -0500

    login and signup fixes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,94 +3,93 @@ const User = require('../../models/user');
 
 // CREATE new user
 router.post('/', async (req, res) => {
-    try {
-      const dbUserData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-      });
-  
-      req.session.save(() => {
-        req.session.loggedIn = true;
-  
-        res.status(200).json(dbUserData);
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  router.delete("/:id", (req, res) => {
-      // delete a user by its `id` value
-      User.destroy({
-        where: {
-          id: req.params.id,
-        },
-      })
-        .then((dbUserData) => {
-          if (!dbUserData) {
-            res.status(404).json({ message: "no user found" });
-            return;
-          }
-          res.json(dbUserData);
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+  try {
+    const dbUserData = await User.create({
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
     });
-  
-  
-  
-  // Login
-  router.post('/login-signup', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-          email: req.body.email,
-        },
-      });
-  
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a user by its `id` value
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
       if (!dbUserData) {
-        res
-          .status(400)
-          .json({ message: 'Invalid email or password.' });
+        res.status(404).json({ message: "no user found" });
         return;
       }
-  
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Invalid email or password.' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.loggedIn = true;
-  
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You have successfully logged in.' });
-      });
-    } catch (err) {
+      res.json(dbUserData);
+    })
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Invalid email or password.' });
+      return;
     }
-  });
-  
-  // Logout
-  router.post('/login-signup', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Invalid email or password.' });
+      return;
     }
-  });
-  
-  module.exports = router;
-  
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You have successfully logged in.' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/login-signup', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -4,7 +4,9 @@ const User = require('../models/user');
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: false });
+  // force: true drops the db before reseeding. when "npm run seed", all data will be replaces with whatever is in the seed files
+  // PREVIOUSLY: await sequelize.sync({ force: false });
+  await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
     individualHooks: true,
