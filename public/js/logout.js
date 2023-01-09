a8399fd80adaf780a0c2216ce74052956c5eb376
jgrant928 <jsgrant928@gmail.com>
2023-01-08 20:33:21 -0500

    fixed search functionality, added logout button

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,4 @@
-const logout = async () => {
+const logoutFormHandler = async () => {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -11,5 +11,6 @@ const logout = async () => {
     }
   };
   
-//   document.querySelector('#logout').addEventListener('click', logout);
-  
+  document
+  .querySelector('#logout-button')
+  .addEventListener('click', logoutFormHandler);
